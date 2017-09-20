ab69d04fbd32695e3f1caa526c7cc75ebcac1a6e
gusdak <gusdak@gmail.com>
2017-09-20 22:45:56 +0300

    20.09

---
+++ Source/TestingGrounds/NPC/ChooseNextWaypoint.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ChooseNextWaypoint.h"
+#include "PatrolRoute.h"
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
+{
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed; }
+
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if (PatrolPoints.Num() == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No patrol points on guard"));
+		return EBTNodeResult::Failed;
+	}
+
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+	
+	Index = (Index + 1) % PatrolPoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, Index);
+
+	return EBTNodeResult::Succeeded;
+}
